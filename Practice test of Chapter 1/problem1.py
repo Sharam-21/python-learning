f3c00a2ad6d6f9f9af23f514e05983b9a30f1754
Sharam-21 <shami63335@gmail.com>
2026-01-18 22:07:36 +0500

    Day 1: Problem 1

---
+++ Practice test of Chapter 1/problem1.py
@@ -0,0 +1,21 @@
+print('''
+"Twinkle, Twinkle, Little Star" was originally a poem titled "The Star", written by Jane Taylor and published in 1806.[1]
+Most people only know the first verse, but here is the full poem:
+The Star
+By Jane Taylor[1][2][3]
+Twinkle, twinkle, little star,
+How I wonder what you are!
+Up above the world so high,
+Like a diamond in the sky.
+When the blazing sun is gone,
+When he nothing shines upon,
+Then you show your little light,
+Twinkle, twinkle, all the night.
+Then the traveler in the dark,
+Thanks you for your tiny spark,
+He could not see which way to go,
+If you did not twinkle so.
+In the dark blue sky you keep,
+And often through my curtains peep,
+For you never shut your eye,
+Till the sun is in the sky.''')
