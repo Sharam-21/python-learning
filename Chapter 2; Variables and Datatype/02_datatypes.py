239a0d92789e8134784ad214fba431eec85460ba
Sharam-21 <shami63335@gmail.com>
2026-01-20 00:10:20 +0500

    Day 2: Data types

---
+++ Chapter 2; Variables and Datatype/02_datatypes.py
@@ -0,0 +1,5 @@
+a = 4           # a is an integer
+b = 8.88        # b is floating point number
+c = "Shami"     # c is string that written in double quotes
+d = True        # d is a boolean variable
+e = None        # e is a None type variable
---
+++ Chapter 2; Variables and Datatype/03_rules-variables.py
@@ -0,0 +1,7 @@
+a = 55
+b = 90.88
+c = "Shami"
+nomi = 77
+_savaiz = 456
+# @aqib = 77   # @ is not accepted
+
