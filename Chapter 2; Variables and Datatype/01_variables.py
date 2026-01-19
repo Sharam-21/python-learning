27155779baa7fc3b3b48b3d45f2f1634710d52f4
Sharam-21 <shami63335@gmail.com>
2026-01-19 23:47:33 +0500

    Day 2 : Variables

---
+++ Chapter 2; Variables and Datatype/01_variables.py
@@ -0,0 +1,15 @@
+a = 2
+b = 6
+# a and b are variables that contain value and work as a container.
+# a and b are also integers
+
+name = "Shami"
+# "" this sign means string. And string is written in double quotes
+
+c = 9.88
+d = 99.65
+# c and d are floating points 
+print(a + b)
+print(name)
+
+print(d - c)
