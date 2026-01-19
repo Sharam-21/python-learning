d4b4ddd23a726495f98f5239cfc7297b68c45f0c
Sharam-21 <shami63335@gmail.com>
2026-01-20 01:05:00 +0500

    Day2: input

---
+++ Chapter 2; Variables and Datatype/06_input.py
@@ -0,0 +1,12 @@
+a = input("Enter number:")
+b = input("Enter number:")
+print(a + b) 
+# if a = 1 and b =2, then answer will be 12. Bcz the data type of variable is string.
+# When 2 string is add. Then answer of both variable is written side by side. Thats why answer is 12.
+
+c = int(input("Enter number 1:"))
+d = int(input("Enter number 2:"))
+print(c + d)
+
+# Now the answer will be integer bcz we used int.
+# Now you put any value the type is integer and answer is right if a +b, the answer is 3.
