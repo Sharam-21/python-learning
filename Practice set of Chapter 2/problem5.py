08ffb93bd443dc757a610170d73030729816e90b
Sharam-21 <shami63335@gmail.com>
2026-01-20 01:28:45 +0500

    Day2: Problem 5

---
+++ Practice set of Chapter 2/problem5.py
@@ -0,0 +1,5 @@
+# Write a python program to fing the average of 2 numbers given by the user.
+
+a = int(input("Enter a number 1: "))
+b = int(input("Enter a number 2:"))
+print("Average of number:", (a +b)/2)
