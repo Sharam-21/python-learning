19e858f8ad740974054c1dcfc57aff8b5f90001f
Sharam-21 <shami63335@gmail.com>
2026-01-20 01:19:37 +0500

    Day2: Problem 3

---
+++ Practice set of Chapter 2/problem3.py
@@ -0,0 +1,6 @@
+# Check the type of varibale assigned using input() function.
+
+a = input("Enter the variable:")
+print(type(a))
+
+# its always a string. Enter integer, floating number, string, none. The will be string only.
