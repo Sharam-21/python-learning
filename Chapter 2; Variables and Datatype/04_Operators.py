a70f23eb4ab7c5ce03325a98a4fb9e83b07a548c
Sharam-21 <shami63335@gmail.com>
2026-01-20 00:38:16 +0500

    Day2: Operators

---
+++ Chapter 2; Variables and Datatype/04_Operators.py
@@ -0,0 +1,52 @@
+# Arithmetic Operators (+, -, *, /)
+a = 3
+b = 20
+c = 90
+d = 45
+print(a + b)
+print(c - d)
+print(a*b)
+print(c/d)
+
+#  Assignment Operators (=, +=, -=)
+e = 8-4            # it means 8-4 and the result value assign to e
+print(e)
+
+f = 4
+f += 5             # it means increament the value of f by 5 and assign to f
+print(f)
+
+g = 8
+g -= 3            # it means decrement the value of g by 3 and assign to g
+print(g)
+
+
+# Comparison Operators(==, <, >, <=, >=, !=)
+h = 6>3          # it means 6 is greater than 3
+print(h)
+
+i = 7<18         # it means 7 is less than 18
+print(i)
+
+j = 5<=5         # it means 5 maybe less than or equal to 5
+print(j)
+
+k = 7>=5          # it means 7 is greater or equal to 5
+print(k)
+
+l = 88!=66       # it means 88 is  not equal to 66
+print(l)
+
+m = 5 == 5       # it means 5 is equal to 5
+print(m)
+
+# Logical Operators (and, not, or)
+ 
+n = True or False   # in "or" operator if 1 is true. the answer will be true.
+print(n)
+
+o = True and False    # in "and" operator of 1 is true. the answer will be false. to get true both are true.
+print(o)
+ 
+print(not(True))          # the "not" operator is change the answer true to false. false to true
+ 
