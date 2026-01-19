8f7b198a948bf2dc6d458c4352bb11789fdeba4f
Sharam-21 <shami63335@gmail.com>
2026-01-20 00:52:54 +0500

    Day2: types

---
+++ Chapter 2; Variables and Datatype/05_types-or-typecasting.py
@@ -0,0 +1,13 @@
+a = 33
+t = type(a)  # type is integer
+print(t)
+
+b = 33.333
+print(type(b))   # type is float
+
+c = "Shami"
+print(type(c))    # type is string
+
+d = float(a)
+print(type(d))   # a is integer but now it is float
+ 
