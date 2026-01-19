5da9b1cca7c897ad3bece998ba92d66a461dcf90
Sharam-21 <shami63335@gmail.com>
2026-01-20 01:33:27 +0500

    Day2: Problem 6

---
+++ Practice set of Chapter 2/problem6.py
@@ -0,0 +1,6 @@
+# Write a python program to calculate the square of the number entered by the user
+
+a = int(input("Enter a number :"))
+print("Square of a number:", a**2)
+
+# In Python " ** ", this sign means square.
