22f9acec0cd3e00897f3db7d3bc5b153cfa30a51
Sharam-21 <shami63335@gmail.com>
2026-01-20 01:14:40 +0500

    Day 2: Problem 2

---
+++ Practice set of Chapter 2/problem2.py
@@ -0,0 +1,15 @@
+# Write a program to find reminder when a number is divided by z.
+a = 44
+b = 3
+print("Reminer when a is divided by b :", a %b)
+# answer: 2
+
+c = 88
+d = 5
+print("Reminder when c is divided by d:", c%d)
+# answer: 3
+
+e = 987
+f = 3
+print("Reminder when e is divided by f:", e%f)
+# answer : 0
