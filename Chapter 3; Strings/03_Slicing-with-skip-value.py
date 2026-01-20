afd379e53f9b451309306f287272423463078fdb
Sharam-21 <shami63335@gmail.com>
2026-01-20 17:15:21 +0500

    Day 3: Slicing with skip value

---
+++ Chapter 3; Strings/03_Slicing-with-skip-value.py
@@ -0,0 +1,6 @@
+a = "123456789"
+print(a[0:7:2])   # it means start from 0 till 7 and 7 is not included. After that jump 2 value after getting a letter.
+# Answer will be  1357
+
+b = "456321789543789553344"
+print(b[0:15:4])       # Answer will be '4297'
