daef739eb524c11963fce8a38291ec0ac9223f16
Sharam-21 <shami63335@gmail.com>
2026-01-20 18:22:13 +0500

    Day 3: Problem 5

---
+++ Practice set of Chapter 3/Problem 5.py
@@ -0,0 +1,9 @@
+# Write a program by using escaoe sequence character.
+
+letter = "Dear Harry,\n \t This python course is nice.\n \t \tThanks!"
+print(letter)
+
+# Return as 
+'''Dear Harry,
+         This python course is nice.
+                Thanks!'''
