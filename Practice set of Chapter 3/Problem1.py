9671aeba06086fd6bca388eb49121eff2c0e8653
Sharam-21 <shami63335@gmail.com>
2026-01-20 18:08:32 +0500

    Day 3: Problem 1

---
+++ Practice set of Chapter 3/Problem1.py
@@ -0,0 +1,10 @@
+# Write a python program to display a user entered name followed by Good Afternoon by using input() Function.
+
+name = input("Enter your name : ")
+print(f"Good Afternoon  {name}")
+
+# In this we use f as fstring and in f string we can right easily a name.
+
+print("Good Afternoon, (name)")
+
+# if u write this it return as same as u can write.
