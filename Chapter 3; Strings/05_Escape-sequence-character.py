3e180b80e0f555b37e8b383f226427f34bf4a95e
Sharam-21 <shami63335@gmail.com>
2026-01-20 17:54:34 +0500

    Day 3: Escape sequence charcters

---
+++ Chapter 3; Strings/05_Escape-sequence-character.py
@@ -0,0 +1,17 @@
+a = "Shami is a good boy \n but some time it acts like a very agressive person."
+# \n is used for new line in the programming language.
+print(a)
+
+b = "Savaiz is a job person \t but sharam is a student."
+# \t is used for giving a tab space in the python.
+print(b)
+
+c = "Quaid-e-Azam is a great leader and said that \"Work, Work, and Work\""
+# \" is used for quote a qutation in python.
+print(c)
+
+
+print("Hello\nWorld")
+print("Name:\t Shami")
+print("It\'s Python")
+print("Smile: \U0001F600")  
