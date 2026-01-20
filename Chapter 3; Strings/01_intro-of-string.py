967e94c147fba2d9bcea96eedcd3ef95f9231d52
Sharam-21 <shami63335@gmail.com>
2026-01-20 16:54:53 +0500

    Day 3: Intro of string

---
+++ Chapter 3; Strings/01_intro-of-string.py
@@ -0,0 +1,18 @@
+name1 = 'Shami'                     # Single quoted string
+name2 = "Savaiz"                    # Double quoted string
+name3 = '''Hafiz Aqib'''            # Triple quoted string
+# 3 String is imputable. Thats means it cannot change.
+
+d = "Sharam"        
+ # this name contain 6 alphabets but in python it shows 5 bcz in programming language counting starts from 0.
+# we can also applying negative counting like -1, -2, -3, -4, -5, -6. And negative counting starts from -1 and on the right side of the string.
+# length of this string is 6. Bcz it contain 6 letters.
+
+# we can also do this 
+print(len(d))              # len means length of the string.
+
+e = d[0:3]          # it means start from 0 till 3. And 3 is excluding.
+print(e)             # it prints 'sha' 
+
+character1 = d[1]        
+print(character1)       # it means print character number 1 of the string d. thats is 'h'.
