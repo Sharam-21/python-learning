e61c3ba3d2e533cf66f7454c1afb5e9f35bf0b6e
Sharam-21 <shami63335@gmail.com>
2026-01-21 01:26:15 +0500

    Day 3: List

---
+++ Chapter 4; List and Tuples/List.py
@@ -0,0 +1,5 @@
+a = ["Apple", "Orange", 33, 6, 78.9, False, "Shami", "Nomi"]
+# We can change in the list but not in the string. Bcz lists are mutable.
+a[0] = "Watermelon"
+print(a[3])
+print(a[0])
