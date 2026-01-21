72432d97b7969f8ac1033bb092a236af1e170e35
Sharam-21 <shami63335@gmail.com>
2026-01-21 17:54:55 +0500

    Day 4: Problem 5

---
+++ Practice set of Chapter 4/Problem5.py
@@ -0,0 +1,9 @@
+# Write a program to count the number of zeros in the following tuple
+
+a = (7, 0, 8, 0, 0, 9)
+print(a.count(0))
+# answer is 3
+
+b = (44, 6, 7, 6, 88, 6, 23, 6, 89, 33, 55, 6, 7, 90, 21, 45, 6)
+print(b.count(6))
+# answer is 6
