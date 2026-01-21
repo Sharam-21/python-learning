9597d12988e484366a19128c0160e9d3228475a7
Sharam-21 <shami63335@gmail.com>
2026-01-21 17:05:57 +0500

    Day 4: Tuples

---
+++ Chapter 4; List and Tuples/Tuples.py
@@ -0,0 +1,8 @@
+a = (1, 3, 4, 6, 78, 67, 21)
+b = ()          # b is a empty tuple
+print(type(a))
+print(type(b)) 
+c = (1,)        # c is tuple that contain only 1 number. 
+d = (1)        # if u write like this. This means type of is integer not tuple.
+
+# U cannot chnage tuple bcz tuple is immutable.
