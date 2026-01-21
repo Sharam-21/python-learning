705e81e916d51a8d2b87f29db955c1b33dce8ce2
Sharam-21 <shami63335@gmail.com>
2026-01-21 17:33:15 +0500

    Day 4: Problem1

---
+++ Practice set of Chapter 4/Problem1.py
@@ -0,0 +1,38 @@
+# Write a program to store seven fruits in the loist entered by the user.
+Fruits = []
+a = input("Enter fruit name 1:")
+Fruits.append(a)
+b = input("Enter fruit name 1:")
+Fruits.append(b)
+c = input("Enter fruit name 1:")
+Fruits.append(c)
+d = input("Enter fruit name 1:")
+Fruits.append(d)
+e = input("Enter fruit name 1:")
+Fruits.append(e)
+f = input("Enter fruit name 1:")
+Fruits.append(f)
+g = input("Enter fruit name 1:")
+Fruits.append(g)
+
+print(Fruits)
+
+
+
+Friends = []
+ss1 = input("Enter your friends name 1:")
+Friends.append(ss1)
+ss2 = input("Enter your friends name 1:")
+Friends.append(ss2)
+ss3 = input("Enter your friends name 1:")
+Friends.append(ss3)
+ss4 = input("Enter your friends name 1:")
+Friends.append(ss4)
+ss5 = input("Enter your friends name 1:")
+Friends.append(ss5)
+ss6 = input("Enter your friends name 1:")
+Friends.append(ss6)
+ss7 = input("Enter your friends name 1:")
+Friends.append(ss7)
+
+print(Friends)
