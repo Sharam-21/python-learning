165e9318e0fa1b06fdda853b50fb17bbf1ffd33e
Sharam-21 <shami63335@gmail.com>
2026-01-21 17:44:57 +0500

    Day 4: Problem 2

---
+++ Practice set of Chapter 4/Problem2.py
@@ -0,0 +1,44 @@
+# Write a program to accept marks of 6 students and display them in a sorted manner.
+
+Marks = []
+student1 = int(input("Enter the marks of student1 :"))
+Marks.append(student1)
+student2 = int(input("Enter the marks of student2 :"))
+Marks.append(student2)
+student3 = int(input("Enter the marks of student3 :"))
+Marks.append(student3)
+student4 = int(input("Enter the marks of student4 :"))
+Marks.append(student4)
+student5 = int(input("Enter the marks of student5 :"))
+Marks.append(student5)
+student6 = int(input("Enter the marks of student6 :"))
+Marks.append(student6)
+
+Marks.sort()
+
+print(Marks)
+
+
+# Write a program to accept diff. margin no. from the user and display in the sorted manner.
+
+Margin = []
+
+number1 = int(input("Enter the number 1 :"))
+Margin.append(number1)
+number2 = int(input("Enter the number 2 :"))
+Margin.append(number2)
+number3 = int(input("Enter the number 3 :"))
+Margin.append(number3)
+number4 = int(input("Enter the number 4 :"))
+Margin.append(number4)
+number5 = int(input("Enter the number 5 :"))
+Margin.append(number5)
+number6 = int(input("Enter the number 6 :"))
+Margin.append(number6)
+number7 = int(input("Enter the number 7 :"))
+Margin.append(number7)
+number8 = int(input("Enter the number 8 :"))
+Margin.append(number8)
+
+Margin.sort()
+print(Margin)
