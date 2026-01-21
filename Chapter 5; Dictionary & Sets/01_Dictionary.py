a278a10fddfd012dbdd37c792dbf66932246219c
Sharam-21 <shami63335@gmail.com>
2026-01-21 19:51:13 +0500

    Day 4: Dictionary

---
+++ Chapter 5; Dictionary & Sets/01_Dictionary.py
@@ -0,0 +1,23 @@
+marks = {"Shami" : 100,
+         "Savaiz" : 90,
+         "Nomi" : 99,
+         "ahmad" : 60,
+         }                       # Syntax of making dictionary.
+                                # Dictionary is mutable means you can chnge the dictionary.
+print(marks, type(marks))
+
+print(marks["Shami"])
+
+
+
+fruits = {  "mango" : 100,
+          "Bannana" : 60,
+          "orange" : 70,
+           "cherry" : 250,
+           "watermelon" : 40,
+           "apple" : 120,
+
+}
+
+print(fruits)
+print(fruits["watermelon"])
