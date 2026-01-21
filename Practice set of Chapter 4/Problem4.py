1684ecc72f84b011c0a3a8fda465c1f76fa48fa8
Sharam-21 <shami63335@gmail.com>
2026-01-21 17:51:14 +0500

    Day 4: Problem 4

---
+++ Practice set of Chapter 4/Problem4.py
@@ -0,0 +1,13 @@
+# Write a program to sum a list of 4 numbers.
+
+a = (22, 55, 66, 88)
+print(sum(a))
+# answer is 231
+
+b = (3, 6, 4, 8, 0, 1)
+print(sum(b))
+# answer is 22
+
+c = (33, 66, 567, 889, 12, 90, 45, 78, 67, 21)
+print(sum(c))
+# answer is 1868
