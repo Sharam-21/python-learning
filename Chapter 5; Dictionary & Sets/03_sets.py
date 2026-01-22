9df5ebed19b17ae440a681f0568556ba9537b0a0
Sharam-21 <shami63335@gmail.com>
2026-01-22 12:38:38 +0500

    Day 5: Sets

---
+++ Chapter 5; Dictionary & Sets/03_sets.py
@@ -0,0 +1,7 @@
+a = set()       # This is called empty set.  In sets elements cannot be repeated.
+
+b = {2, 4, 66, 67, 87, 21, 98, 0, 0, 0, 0}
+print(b) 
+
+# it print : {0, 2, 67, 4, 66, 98, 21, 87}
+# Bcz set is unorded and connot repeat a number.
