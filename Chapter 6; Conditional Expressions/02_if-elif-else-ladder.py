46bba909987f1ab01d501a87a3e0307b555939d8
Sharam-21 <shami63335@gmail.com>
2026-01-22 19:11:54 +0500

    Day 5: Conditional

---
+++ Chapter 6; Conditional Expressions/02_if-elif-else-ladder.py
@@ -0,0 +1,16 @@
+a = int(input("Enter your age:"))
+
+
+if(a>=18):
+    print("You are above the age of consent.")
+    print("Good for you ")
+
+elif(a<0):
+    print("You entered invalid age.")  
+
+elif(a==0):
+    print("You entered 0, which is one valid age.")      
+
+    
+else: 
+    print("You are below the age of consent.")  
