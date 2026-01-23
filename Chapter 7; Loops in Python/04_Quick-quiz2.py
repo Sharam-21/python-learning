e90b8aa9b6ea2b1aad044b8b097f9756c6083bb6
Sharam-21 <shami63335@gmail.com>
2026-01-23 17:57:01 +0500

    Day 6: Quick quiz 2

---
+++ Chapter 7; Loops in Python/04_Quick-quiz2.py
@@ -0,0 +1,22 @@
+#  Write a program to print the content of a list using while loops. 
+
+list =[2, 5, 7, "Shami", "Savaiz", "Aqib", "Nomi", "Zubair", "Usama"]
+
+a = 0
+while(a<len(list)):
+    print(list[a])
+    a = a +1
+
+
+
+'''Output of this loop is :
+2
+5
+7
+Shami
+Savaiz
+Aqib
+Nomi
+Zubair
+Usama    '''
+
