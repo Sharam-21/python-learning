a49f3c3eb2b52cddcd09d7fbb47bd4855d9955b1
Sharam-21 <shami63335@gmail.com>
2026-01-23 07:12:58 +0500

    Day 6: Problem 2

---
+++ Practice set of Chapter 6/Problem 2.py
@@ -0,0 +1,27 @@
+'''Write a program to find out whether a student has passed or failed if it requires a
+total of 40% and at least 33% in each subject to pass. Assume 3 subjects and
+take marks as an input from the user.'''
+
+
+marks1 = int(input("ENter the marks of subject 1 :"))
+marks2 = int(input("ENter the marks of subject 2 :"))
+marks3 = int(input("ENter the marks of subject 3 :"))
+
+
+#  check for total percentage
+
+total_pecentage = (100) * (marks1 + marks2 + marks3)/300
+
+if(total_pecentage>=40 and marks1>=33 and marks2>=33 and marks3>=33):
+    print("You are Pass", total_pecentage)
+
+else:
+    print("You are Fail, Try again next year.", total_pecentage)
+
+
+
+
+    
+
+
+
