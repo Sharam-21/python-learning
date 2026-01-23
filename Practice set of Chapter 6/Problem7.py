c75a37e505c5664f7c72d5fc291f38d2b26dc4e3
Sharam-21 <shami63335@gmail.com>
2026-01-23 08:02:34 +0500

    Day 6: Problem 7

---
+++ Practice set of Chapter 6/Problem7.py
@@ -0,0 +1,20 @@
+# Write a program to find out whether a given post is talking about “Harry” or not.
+
+post = input("Enter your post:")
+
+if("Shami".lower() in post.lower()):
+    print("This post is talking about shami.")
+
+
+else:
+    print("This post is not talking about shami")    
+
+
+
+'''Enter your post:shami is a good boy
+This post is talking about shami.
+
+
+
+Enter your post:savaiz is a good boy
+This post is not talking about shami  '''  
