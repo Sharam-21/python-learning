783f94b9dc97ab2c46977afef1c4d4c91600df30
Sharam-21 <shami63335@gmail.com>
2026-01-23 18:29:44 +0500

    Day 6: For loop with else

---
+++ Chapter 7; Loops in Python/07_for-loop-with-else.py
@@ -0,0 +1,34 @@
+
+
+l= [1,7,8]
+for item in l:
+  print(item)
+else:
+  print("done") # this is printed when the loop exhausts!
+
+'''Output is this :
+1
+7
+8
+done
+'''
+
+
+a = (2, 5, 7, 9, "Shami", "Orange", "Burger")
+
+for number in a :
+  print(number)
+
+else:
+  print("Done.")  
+
+'''Output is this :
+2
+5
+7
+9
+Shami
+Orange
+Burger
+Done.'''
+
